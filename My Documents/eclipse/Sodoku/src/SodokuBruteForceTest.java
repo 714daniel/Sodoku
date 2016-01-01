788fbef3a3fb053d775eb2b8e6300688c0f00a5d
714daniel <714.daniel.smith@gmail.com>
2016-01-01 13:52:17 -0800

    changed all formulas

--- My Documents/eclipse/Sodoku/src/SodokuBruteForceTest.java
+++ My Documents/eclipse/Sodoku/src/SodokuBruteForceTest.java
@@ -1,27 +1,33 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class SodokuBruteForceTest {
+	public static ArrayList<Integer> zer = new ArrayList<>();
 	public static void main(String[] args) throws FileNotFoundException {
 		SodokuGrid s = new SodokuGrid(9);
 		File f = new File(
-				"H:\\My Documents\\eclipse\\Sodoku\\src\\sodokuProb.txt");
+				"My Documents\\eclipse\\Sodoku\\src\\sodokuProb.txt");
+
+
 		s.fillGrid(f);
-		printSolutions(s);
+		System.out.println(getSolutionFinal(s, printSolutions(s)));
 		//System.out.println(Arrays.deepToString(s.getSubSquares()));
 	}
 
 	public static boolean[][][] solve(SodokuGrid s) {
 		// Already known grid info
-		Integer[][] grid = s.getGrid();
+		int[][] grid = s.getGrid();
 		// Stores true if loc at x,y can hold value
 		boolean[][][] possible = new boolean[s.getLength()][s.getLength()][s
 				.getLength() + 1];
 		for (int x = 0; x < s.getLength(); x++) {
 			for (int y = 0; y < s.getLength(); y++) {
 				if (grid[x][y] == 0) {
-					for (int tryAll = 1; tryAll <= s.getLength(); tryAll++) {
+				zer.add(x * 10 + y);
+						for (int tryAll = 1; tryAll <= s.getLength(); tryAll++) {
 						possible[x][y][tryAll] = s.tryWith(x, y, tryAll);
 					}
 				}
@@ -31,19 +37,43 @@ public class SodokuBruteForceTest {
 		return possible;
 	}
 
-	public static void printSolutions(SodokuGrid s) {
+	public static ArrayList<ArrayList<Integer>> printSolutions(SodokuGrid s) {
+		ArrayList<ArrayList<Integer>> solu = new ArrayList<ArrayList<Integer>>();
 		boolean[][][] sol = solve(s);
 		for (int x = 0; x < s.getLength(); x++) {
 			for(int y = 0; y < s.getLength(); y++) {
-				if(s.getNum(x,y) == 0) {
-					for(int i = 1; i<10; i++) {
+					if(s.getNum(x,y) == 0) {
+					solu.add(new ArrayList<Integer>());
+						for(int i = 1; i<10; i++) {
 						if(sol[x][y][i])
-						System.out.print(i);
+							solu.get(solu.size() - 1).add(i);
 					}
 				System.out.println();
 				}
 			}
 		}
 
+	return solu;
 	}
+
+	public static int[][] getSolutionFinal(SodokuGrid s,java.util.ArrayList<java.util.ArrayList<java.lang.Integer>> solu ) {
+
+
+
+		return getRecSolution(s, solu, 0);
+	}
+
+	public static int[][] getRecSolution(SodokuGrid s,ArrayList<ArrayList<Integer>> solu, int zeroInd) {
+		if(zeroInd == solu.size()) {
+				return s.getGrid();
+			}
+		for(Integer i:solu.get(zeroInd)) {
+			if (s.tryWith(zer.get(zeroInd / 10), zer.get(zeroInd % 10), i)) {
+				System.out.println(zeroInd);
+				getRecSolution(s.setTo(zer.get(zeroInd / 10), zer.get(zeroInd % 10), i), solu, zeroInd++);
+			}
+		}
+		return null;
+	}
+
 }
--- My Documents/eclipse/Sodoku/src/SodokuGrid.java
+++ My Documents/eclipse/Sodoku/src/SodokuGrid.java
@@ -5,119 +5,132 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class SodokuGrid {
-	private int length;
-	private static Integer[][] grid;
-	public static ArrayList<Integer> squares;
-	private static int[][] subSquares;
+    private int length;
+    private static int[][] grid;
+    public static ArrayList<Integer> squares;
+    private static int[][] subSquares;
 
-	public SodokuGrid(int length) {
-		this.length = length;
-		grid = new Integer[length][length];
-		squares = new ArrayList<Integer>();
-	}
+    public SodokuGrid(int length) {
+        this.length = length;
+        grid = new int[length][length];
+        squares = new ArrayList<Integer>();
+    }
 
-	public void fillGrid(File f) throws FileNotFoundException {
-
-		Scanner sc = new Scanner(f);
-		for (int x = 0; x < length; x++) {
-			for (int y = 0; y < length; y++) {
-				grid[x][y] = sc.nextInt();
-			}
-		}
-		sc.close();
-		subSquares = solveSubSquares();
-
-	}
-
-	public Integer[][] getGrid() {
-		return grid;
-
-	}
-
-	public int getNum(int x, int y) {
-		return grid[x][y];
-	}
-
-	public int getLength() {
-		return length;
-	}
-
-	public int[][] getSubSquares() {
-		return subSquares;
-	}
-
-	public boolean tryWith(int x, int y, int val) {
-		int getXsub = x;
-		int getYsub = y;
-		int ss = 0;
-		while (getXsub - 3 > 0 || getYsub - 3 > 0) {
-			if (getXsub - 3 > 0) {
-				getXsub -= 3;
-				ss++;
-			} else if (getYsub - 3 > 0) {
-				getYsub -= 3;
-				ss += 3;
-			}
-		}
-		boolean unique = true;
-		for (int x1 = 0; x1 < length; x1++) {
-			if (grid[x1][y] == val && x1 != x)
-				return false;
-		/*	if (subSquares[ss][x1] == grid[x1][y] && !unique)
-				return false;
-			else if(subSquares[ss][x1] == grid[x1][y])
-				unique = false;
-				*/
-		}
-
-		for (int y1 = 0; y1 < length; y1++) {
-			if (grid[x][y1] == val && y1 != y)
-				return false;
-		}
-
-		return true;
-
-	}
-
-	public int[][] solveSubSquares() {
-		int x = 0;
-
-		for (int y = 0; y < 9; y++) {
-			squares.add(grid[x % 3][y]);
-			x++;
-			squares.add(grid[x % 3][y]);
-			x++;
-			squares.add(grid[x % 3][y]);
-			x++;
-		}
-
-		for (int y = 0; y < 9; y++) {
-			squares.add(grid[x % 3 + 3][y]);
-			x++;
-			squares.add(grid[x % 3 + 3][y]);
-			x++;
-			squares.add(grid[x % 3 + 3][y]);
-			x++;
-		}
-
-		for (int y = 0; y < 9; y++) {
-			squares.add(grid[x % 3 + 6][y]);
-			x++;
-			squares.add(grid[x % 3 + 6][y]);
-			x++;
-			squares.add(grid[x % 3 + 6][y]);
-			x++;
-		}
-		int[][] subSquares = new int[9][9];
-		int index = 0;
-		for (int i = 0; i < 9; i++) {
-			for (int v = 0; v < 9; v++) {
-				subSquares[i][v] = squares.get(index);
-				index++;
-			}
-		}
-
-		return subSquares;
-
-	}
+    public SodokuGrid(int[][] grid) {
+    this.grid = grid;
+    squares = new ArrayList<Integer>();
+    this.length = 9;
 }
+
+    public void fillGrid(File f) throws FileNotFoundException {
+
+        Scanner sc = new Scanner(f);
+        for (int x = 0; x < length; x++) {
+            for (int y = 0; y < length; y++) {
+                grid[x][y] = sc.nextInt();
+            }
+        }
+        sc.close();
+        subSquares = solveSubSquares();
+
+
+    }
+
+    public int[][] getGrid() {
+        return grid;
+
+    }
+
+    public int getNum(int x, int y) {
+        return grid[x][y];
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public int[][] getSubSquares() {
+        return subSquares;
+    }
+    public SodokuGrid setTo(int x, int y, int val) {
+        grid[x][y] = val;
+        return this;
+    }
+
+    public boolean tryWith(int x, int y, int val) {
+        int getXsub = x;
+        int getYsub = y;
+        int ss = 0;
+        while (getXsub - 3 > 0 || getYsub - 3 > 0) {
+            if (getXsub - 3 > 0) {
+                getXsub -= 3;
+                ss++;
+            } else if (getYsub - 3 > 0) {
+                getYsub -= 3;
+                ss += 3;
+            }
+        }
+        boolean unique = true;
+        for (int x1 = 0; x1 < length; x1++) {
+            if (grid[x1][y] == val && x1 != x)
+                return false;
+        }
+
+        for (int y1 = 0; y1 < length; y1++) {
+            if (grid[x][y1] == val && y1 != y)
+                return false;
+        }
+
+        return true;
+
+    }
+
+    public int[][] solveSubSquares() {
+        int x = 0;
+
+        for (int y = 0; y < 9; y++) {
+            squares.add(grid[x % 3][y]);
+            x++;
+            squares.add(grid[x % 3][y]);
+            x++;
+            squares.add(grid[x % 3][y]);
+            x++;
+        }
+
+        for (int y = 0; y < 9; y++) {
+            squares.add(grid[x % 3 + 3][y]);
+            x++;
+            squares.add(grid[x % 3 + 3][y]);
+            x++;
+            squares.add(grid[x % 3 + 3][y]);
+            x++;
+        }
+
+        for (int y = 0; y < 9; y++) {
+            squares.add(grid[x % 3 + 6][y]);
+            x++;
+            squares.add(grid[x % 3 + 6][y]);
+            x++;
+            squares.add(grid[x % 3 + 6][y]);
+            x++;
+        }
+        int[][] subSquares = new int[9][9];
+        int index = 0;
+        for (int i = 0; i < 9; i++) {
+            for (int v = 0; v < 9; v++) {
+                subSquares[i][v] = squares.get(index);
+                index++;
+            }
+        }
+
+        return subSquares;
+    }
+    public boolean isValid() {
+    return true;
+    }
+
+
+}
+
+
+
